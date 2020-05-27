2f0ee186ccbf4a3abe3f1fb127600b021a4841b8
nava20ir <59726898+nava20ir@users.noreply.github.com>
2020-05-27 12:37:01 +0300

    Update 8_7_time_dose_plots_heatplot.R

--- scripts/8_7_time_dose_plots_heatplot.R
+++ scripts/8_7_time_dose_plots_heatplot.R
@@ -3,12 +3,24 @@
 
 
 rm(list=ls())
-load('outputData/plot_data/time_exposure.RData')
-load('outputData/plot_data/Dose_response.RData')
-
-lin_lvls_dose<-factor(c('No-EDC < No-EDC < EDC ','No-EDC < EDC = EDC','EDC < EDC < EDC','EDC = EDC = EDC'))
-
+library(ggpubr)
 library(RColorBrewer)
+load('outputData/plot_data/Dose_response.RData')
+load('outputData/plot_data/time_exposure.RData')
+
+lin_lvls_dose<-factor(c('No_EDC - No_EDC < EDC', #G1
+                        'No_EDC < EDC - EDC',    #G2
+                        'EDC < EDC <= EDC',      #G3
+                        'EDC - EDC - EDC'))      #G4   dose levels
+
+lin_lvls_time<-factor(c('No_EDC - No_EDC < EDC', #G1
+                        'No_EDC < EDC - EDC',    #G2
+                        'EDC < EDC <= EDC',      #G3
+                        'EDC - EDC - EDC',       #G4
+                        'EDC - EDC > No_EDC'     #G5 time levels
+                        ))      
+
+
 p1<-ggplot(dose_final, 
        aes(x = factor(data_layer,levels = c('Low','Middle','High')), 
            y = len, colour=factor(pattern,levels = lin_lvls_dose)))+
@@ -29,7 +41,6 @@ p1<-ggplot(dose_final,
         axis.text.y = element_text(size=12),
         panel.spacing = unit(2,'lines'))
 
-lin_lvls_time<-factor(c('No-EDC < No-EDC < EDC ','No-EDC < EDC = EDC','EDC > EDC > No-EDC','EDC < EDC < EDC','EDC = EDC = EDC'))
 time_final$data_layer<-gsub(x=time_final$data_layer,pattern = '_',replacement = ' ')
 p2<-ggplot(time_final, 
            aes(x = factor(data_layer,levels = c('8 days','15 days','29 days')), 
@@ -52,152 +63,87 @@ p2<-ggplot(time_final,
 
 g1<-ggarrange(p1,p2,ncol = 2,common.legend = F,legend = 'bottom')
 print(g1)
-# #rm(list = ls())
-# ## Dose Bio markers
-# load('outputData/plot_data/dose_bio_marker_scenario_3.RData')
-# source('functions/annotation_functions.R')
-# #dose_biomarker$pathway<-pathway_annotate(dose_biomarker$pathway)
-# dose_biomarker$Dose<-gsub(dose_biomarker$Dose,pattern = 'low',replacement = 'Low')
-# dose_biomarker$Dose<-gsub(dose_biomarker$Dose,pattern = 'middle',replacement = 'Middle')
-# dose_biomarker$Dose<-gsub(dose_biomarker$Dose,pattern = 'high',replacement = 'High')
-# #library(RColorBrewer)
-# p3<-ggplot(dose_biomarker, aes(x=factor(Dose,levels = c('Low','Middle','High')),
-#                            y=activation_score,colour=factor(pathway))) +
-#   geom_point(show.legend = F)+
-#   geom_line(aes(group=pathway),show.legend = F)+
-#   #guides(col=guide_legend(nrow = 2))+
-#   labs(colour='Pathways')+
-#   #scale_color_manual(values=rev(brewer.pal(n = 4, name ='RdYlGn')))+
-#   #scale_color_gradient(low='yellow',high = 'red')+
-#   xlab('TG_GATEs_Repeated_Dose')+ylab('Pathway Activation Scores')+ theme_minimal()+
-#   theme(legend.text = element_text(size=10),
-#         axis.title.x = element_blank(),
-#         legend.position = 'right',axis.text.x = element_text(size=16),
-#         axis.title = element_text(size = 16),axis.text.y = element_text(size=16),
-#         panel.spacing = unit(2,'lines'))
-
-# 
-# 
-# 
-# ##Time Bio markers plot of the most significant pathways after exposure based on doses and time points
-# load('outputData/plot_data/time_bio_marker_scenario5.RData')
-# source('functions/annotation_functions.R')
-# #time_biomarker$pathway<-pathway_annotate(time_biomarker$pathway)
-# time_biomarker$Dose<-gsub(time_biomarker$Dose,pattern = 'eight_day',replacement = '8 day')
-# time_biomarker$Dose<-gsub(time_biomarker$Dose,pattern = 'fifteen_day',replacement = '15 day')
-# time_biomarker$Dose<-gsub(time_biomarker$Dose,pattern = 'twentynine_day',replacement = '29 day')
-# #library(RColorBrewer)
-# p4<-ggplot(time_biomarker, aes(x=factor(Dose,levels = c('8 day','15 day','29 day')),
-#                            y=activation_score,colour=factor(pathway))) +
-#   geom_point(show.legend = F)+
-#   geom_line(aes(group=pathway),show.legend = F)+
-#   #guides(col=guide_legend(nrow = 2))+
-#   labs(colour='Pathways')+
-#   #scale_color_manual(values=rev(brewer.pal(n = 4, name ='RdYlGn')))+
-#   #scale_color_gradient(low='yellow',high = 'red')+
-#   xlab('TG_GATEs_Repeated_Dose')+ylab('Pathway Activation Scores')+ theme_minimal()+
-#   theme(legend.text = element_text(size=10),
-#         axis.title.x = element_blank(),
-#         legend.position = 'right',axis.text.x = element_text(size=16),
-#         axis.title = element_text(size = 16),axis.text.y = element_text(size=16),
-#         panel.spacing = unit(2,'lines'))
-# ggarrange(p3,p4,ncol = 2,common.legend = F,legend = 'right')
 
 
-# 2. Heatplot of Significant pathways time of exposure 8,15,29 days TG-Gates-------------------------------------
-library('org.Hs.eg.db')
-library(ggplot2)
-source('functions/annotation_functions.R')
-load('outputData/plot_data/time_bio_marker_all_increase_1_2_5_scenarios.RData') #script 9_6_2
-time_biomarker<-data;rm(data)
 
-load('outputData/toxdb2gene_final.RData')
-significant_pathwawys<-unique(time_biomarker$pathway)
-selected_list_pathways<-moa_pathways[significant_pathwawys]
-selected_list_pathways<-selected_list_pathways[which(sapply(selected_list_pathways,length)<10)] # we select the pathways with the number of genes less than 10
+# 2. new approach with preselection based on ROC curve Heatplot of Significant pathways for doses and time of exposure 8,15,29 days TG-Gates-------------------------------------
 
-names(selected_list_pathways)<-pathway_annotate(names(selected_list_pathways))
-selected_pathway_list_symbol<-sapply(selected_list_pathways, function(x)mapIds(org.Hs.eg.db, x,  'SYMBOL','ENTREZID'))
-all_genes<-unique(unlist(selected_pathway_list_symbol))
-load('inputData/text_mining/endocrine_disruption_result.RData')
-gene_freq$entrez<-as.character(gene_freq$entrez)
-gene_freq$entrez<-mapIds(org.Hs.eg.db, gene_freq$entrez,  'SYMBOL','ENTREZID')
-ind<-which(gene_freq$entrez %in% intersect(gene_freq$entrez,all_genes))
-pubgenes=gene_freq$Freq[ind]
-names(pubgenes)<-gene_freq$entrez[ind]
-DEGs<-read.csv(file='inputData/DEGs.csv',header = T,stringsAsFactors = F)
-DEGs<-DEGs$Gene[which(DEGs$Organism=='Homo sapiens')]
-Deg_genes<-intersect(all_genes,DEGs)
-Deg_genes<-Deg_genes[!Deg_genes %in% names(pubgenes)]
-Deg_list<-rep(0,length(Deg_genes))
-names(Deg_list)<-Deg_genes
-#heatplot.enrichResult(selected_pathway_list_symbol,c(pubgenes,Deg_list))
-citation<-c(pubgenes,Deg_list)
-ldf <- lapply(1:length(selected_pathway_list_symbol), function(i) {
-  data.frame(categoryID=rep(names(selected_pathway_list_symbol[i]),
-                            length(selected_pathway_list_symbol[[i]])),
-             Gene=selected_pathway_list_symbol[[i]])
-})
-d<-do.call('rbind', ldf)
-d$citation <- citation[as.character(d[,2])]
-time_heatplot<-d
-save(time_heatplot,file = 'outputData/plots_tables_functions/Dose_response/time_heatplot.RData')
+heat_plot<-function(significant_pathwawys,list_groups){
+  load('outputData/toxdb2gene_final.RData')
+  pathsize<-as.data.frame(sapply(moa_pathways, length))
+  colnames(pathsize)<-"length_pathway"
+  pathsize$categoryID<-rownames(pathsize)
+  source('functions/annotation_functions.R')
+  library('org.Hs.eg.db')
+  library(ggplot2)
+  selected_list_pathways<-moa_pathways[significant_pathwawys]
+  #selected_list_pathways<-selected_list_pathways[which(sapply(selected_list_pathways,length)<10)] # we select the pathways with the number of genes less than 10
+  selected_pathway_list_symbol<-sapply(selected_list_pathways, function(x)mapIds(org.Hs.eg.db, x,  'SYMBOL','ENTREZID'))
+  all_genes<-unique(unlist(selected_pathway_list_symbol))
+  load('inputData/text_mining/endocrine_disruption_result.RData') # text mining results
+  gene_freq$entrez<-as.character(gene_freq$entrez)
+  gene_freq$entrez<-mapIds(org.Hs.eg.db, gene_freq$entrez,  'SYMBOL','ENTREZID')
+  ind<-which(gene_freq$entrez %in% intersect(gene_freq$entrez,all_genes))
+  pubgenes=gene_freq$Freq[ind]
+  names(pubgenes)<-gene_freq$entrez[ind]
+  DEGs<-read.csv(file='inputData/DEGs.csv',header = T,stringsAsFactors = F) # essential genes
+  DEGs<-DEGs$Gene[which(DEGs$Organism=='Homo sapiens')]
+  essential_genes<-intersect(all_genes,DEGs)
+  Deg_genes<-essential_genes[!essential_genes %in% names(pubgenes)]
+  Deg_list<-rep(0,length(Deg_genes))
+  names(Deg_list)<-Deg_genes
+  citation<-c(pubgenes,Deg_list)
+  ldf <- lapply(1:length(selected_pathway_list_symbol), function(i) {
+    data.frame(categoryID=rep(names(selected_pathway_list_symbol[i]),
+                              length(selected_pathway_list_symbol[[i]])),
+               Gene=selected_pathway_list_symbol[[i]])
+  })
+  heatplot<-do.call('rbind', ldf)
+  heatplot$citation <- citation[as.character(heatplot[,2])]
+  heatplot$group<-'Group'
+  for (i in 1:length(list_groups)){
+    for (j in 1:nrow(heatplot)){
+      if (as.character(heatplot$categoryID[j]) %in% list_groups[[i]])heatplot$group[j]<-paste(heatplot$group[j],i,sep = '_')
+      
+    }
+  }
+  heatplot$essential<-heatplot$Gene %in% essential_genes
+  heatplot<-merge(heatplot,pathsize,all.x=T)
+  heatplot$categoryID<-pathway_annotate(as.character(heatplot$categoryID))
+  heatplot$categoryID<-paste(heatplot$categoryID,heatplot$group,sep = '_')
+  # print(ggplot(heatplot, aes_(~Gene, ~categoryID)) +
+  #         geom_tile(aes_(fill = ~citation), color = "white") +
+  #         scale_fill_continuous(low="orange", high="blue", name = "Pubmed Citations")+
+  #         xlab(NULL) + ylab(NULL) + theme_minimal() +
+  #         theme(panel.grid.major = element_blank(),
+  #               axis.text.x=element_text(angle = 60, hjust = 1)))
+  return(heatplot)
+}
 
-p3<-ggplot(d, aes_(~Gene, ~categoryID)) +
+load('outputData/new_time_dose_sensitivity/ANOVA_Times.RData')
+time_significant_pathways_uniq<-unique(do.call(c,time_significant_pathways))
+time_heat_plot_data<-heat_plot(time_significant_pathways_uniq,time_significant_pathways)
+
+load('outputData/new_time_dose_sensitivity/ANOVA_DOSEs.RData')
+dose_significant_pathways_uniq<-unique(do.call(c,dose_significant_pathways))
+dose_heat_plot_data<-heat_plot(dose_significant_pathways_uniq,dose_significant_pathways)
+
+save(dose_heat_plot_data,time_heat_plot_data,file = 'outputData/new_time_dose_sensitivity/heatplot_data.RData')
+write.csv(dose_heat_plot_data,file = 'outputData/excel_files/dose_sensitive_TG_gates_pathways.csv')
+write.csv(time_heat_plot_data,file = 'outputData/excel_files/time_sensitive_TG_gates_pathways.csv')
+
+# visualize the plot
+load('outputData/new_time_dose_sensitivity/heatplot_data.RData')
+dose_heat_plot_data<-dose_heat_plot_data[dose_heat_plot_data$length_pathway<=10,]
+time_heat_plot_data<-time_heat_plot_data[time_heat_plot_data$length_pathway<=10,]
+plot_heat_pl<-function(heat_plot_data){
+  require(ggplot2)
+  ggplot(heat_plot_data, aes_(~Gene, ~categoryID)) +
     geom_tile(aes_(fill = ~citation), color = "white") +
-     scale_fill_continuous(low="orange", high="blue", name = "Pubmed Citations")+
-     xlab(NULL) + ylab(NULL) + theme_minimal() +
-     theme(panel.grid.major = element_blank(),
-           axis.text.x=element_text(angle = 60, hjust = 1))
- 
+    scale_fill_continuous(low="orange", high="blue", name = "Pubmed Citations")+
+    xlab(NULL) + ylab(NULL) + theme_minimal() +
+    theme(panel.grid.major = element_blank(),
+          axis.text.x=element_text(angle = 60, hjust = 1))
+}
+lapply(list(dose_heat_plot_data,time_heat_plot_data), function(x)plot_heat_pl(x))
 
-# 3. Heatplot of significant pathways dose exposure Low, Middle and High for TG-Gates ---------------------
-
-
-library('org.Hs.eg.db')
-library(ggplot2)
-source('functions/annotation_functions.R')
-
-load('outputData/plot_data/dose_bio_marker_all_increase_scenario_1_2_3.RData')
-dose_biomarker<-data;rm(data)
-load('outputData/toxdb2gene_final.RData')
-significant_pathwawys<-unique(dose_biomarker$pathway)
-selected_list_pathways<-moa_pathways[significant_pathwawys]
-selected_list_pathways<-selected_list_pathways[which(sapply(selected_list_pathways,length)<10)]
-
-names(selected_list_pathways)<-pathway_annotate(names(selected_list_pathways))
-selected_pathway_list_symbol<-sapply(selected_list_pathways, function(x)mapIds(org.Hs.eg.db, x,  'SYMBOL','ENTREZID'))
-all_genes<-unique(unlist(selected_pathway_list_symbol))
-load('inputData/text_mining/endocrine_disruption_result.RData')
-gene_freq$entrez<-as.character(gene_freq$entrez)
-gene_freq$entrez<-mapIds(org.Hs.eg.db, gene_freq$entrez,  'SYMBOL','ENTREZID')
-ind<-which(gene_freq$entrez %in% intersect(gene_freq$entrez,all_genes))
-pubgenes=gene_freq$Freq[ind]
-names(pubgenes)<-gene_freq$entrez[ind]
-DEGs<-read.csv(file='inputData/DEGs.csv',header = T,stringsAsFactors = F)
-DEGs<-DEGs$Gene[which(DEGs$Organism=='Homo sapiens')]
-Deg_genes<-intersect(all_genes,DEGs)
-Deg_genes<-Deg_genes[!Deg_genes %in% names(pubgenes)]
-Deg_list<-rep(0,length(Deg_genes))
-names(Deg_list)<-Deg_genes
-#heatplot.enrichResult(selected_pathway_list_symbol,c(pubgenes,Deg_list))
-citation<-c(pubgenes,Deg_list)
-ldf <- lapply(1:length(selected_pathway_list_symbol), function(i) {
-  data.frame(categoryID=rep(names(selected_pathway_list_symbol[i]),
-                            length(selected_pathway_list_symbol[[i]])),
-             Gene=selected_pathway_list_symbol[[i]])
-})
-d<-do.call('rbind', ldf)
-d$citation <- citation[as.character(d[,2])]
-dose_heatplot<-d
-save(dose_heatplot,file = 'outputData/plots_tables_functions/Dose_response/dose_heatplot.RData')
-p4<-ggplot(d, aes_(~Gene, ~categoryID)) +
-  geom_tile(aes_(fill = ~citation), color = "white") +
-  scale_fill_continuous(low="orange", high="blue", name = "Pubmed Citations")+
-  xlab(NULL) + ylab(NULL) + theme_minimal() +
-  theme(panel.grid.major = element_blank(),
-        axis.text.x=element_text(angle = 60, hjust = 1))
-
- g2<-ggarrange(p4,p3,nrow = 2,common.legend = T,legend = 'bottom')
-print(g2)
-ggarrange(g1,g2,nrow = 2,common.legend = F,legend = 'bottom')
